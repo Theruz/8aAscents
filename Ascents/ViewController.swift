96941da0ec4a54e0b159bbfd0768795f8eccc83a
Theophile Ruzicka <theophile.ruzicka@gmail.com>
2017-06-09 18:04:14 +0200

    initial commit

---
+++ Ascents/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ascents
+//
+//  Created by Theophile on 09.06.17.
+//  Copyright © 2017 Theophile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
